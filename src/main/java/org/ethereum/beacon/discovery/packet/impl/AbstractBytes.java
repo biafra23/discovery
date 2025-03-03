4f1275f65afaf68e5ce717df95ddeecd9733fccb
Dirk Jäckel <github@dirk.jaeckel.name>
2025-03-03 16:59:56 +0000

    log added in checkMinSize

--- src/main/java/org/ethereum/beacon/discovery/packet/impl/AbstractBytes.java
+++ src/main/java/org/ethereum/beacon/discovery/packet/impl/AbstractBytes.java
@@ -20,7 +20,7 @@ public abstract class AbstractBytes implements BytesSerializable {
       throw new DecodeException(
           "FORK: Data size (" + bytes.size() + ") doesn't match expected: " + expectedSize);
     }
-    LOG.warn("FORK: This is the forked version of discovery");
+    LOG.warn("FORK: bytes.size(): (" + bytes.size() + ") == expectedSize: (" + expectedSize + ")");
     return bytes;
   }
 
@@ -29,6 +29,7 @@ public abstract class AbstractBytes implements BytesSerializable {
       throw new DecodeException(
           "Data is too small: " + bytes.size() + ", (expected at least " + minimalSize + " bytes)");
     }
+    LOG.warn("FORK: {} < {}", bytes.size(), minimalSize);
     return bytes;
   }
 
