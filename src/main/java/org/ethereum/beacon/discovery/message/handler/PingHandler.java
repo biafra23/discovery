b390f5d4210a706338971088af79a2fa81e3bdf1
Dirk Jäckel <github@dirk.jaeckel.name>
2025-03-05 22:32:39 +0000

    remove Logger

--- src/main/java/org/ethereum/beacon/discovery/message/handler/PingHandler.java
+++ src/main/java/org/ethereum/beacon/discovery/message/handler/PingHandler.java
@@ -10,13 +10,10 @@ import org.ethereum.beacon.discovery.message.PingMessage;
 import org.ethereum.beacon.discovery.message.PongMessage;
 import org.ethereum.beacon.discovery.schema.NodeRecord;
 import org.ethereum.beacon.discovery.schema.NodeSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class PingHandler implements MessageHandler<PingMessage> {
 
   private final EnrUpdateTracker enrUpdateTracker;
-  private static final Logger LOG = LoggerFactory.getLogger(PingHandler.class);
 
   public PingHandler(final EnrUpdateTracker enrUpdateTracker) {
     this.enrUpdateTracker = enrUpdateTracker;
